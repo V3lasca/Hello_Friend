e7098dd8e599f99d6317d474864a68f0811aea91
V3lasc <109042362+V3lasc@users.noreply.github.com>
2022-07-26 19:13:59 -0400

    Add files via upload

---
+++ hellofriend.mov.cmd
@@ -0,0 +1,218 @@
+@echo off
+
+rem Maximized window
+if not "%1"=="max" start /max cmd /c %0 max & exit /b
+
+title /
+
+color 03
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                  Hello friend...
+pause > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                  Hello, friend?
+pause > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                   That's lame...
+pause > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                          Maybe I should give you a name...
+pause > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                         But let me stop this silly game...
+pause > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                          So that I may open your world...
+pause > nul
+cls 
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+rem Your Call
+echo                                                                              Are you a '1' or a '0'? 
+set /p anwser=~#                                                                                                                                                                                                                                                                  
+cls
+if %anwser%==1 goto 1
+if %anwser%==one goto 1
+if not %anwerser%==1 goto 0
+
+:1 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                      LOADING
+timeout /t 1 /nobreak > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                      LOADING.
+timeout /t 1  /nobreak > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                      LOADING..
+timeout /t 1 /nobreak > nul
+cls
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                                      LOADING...
+timeout /t 1 /nobreak > nul
+cls
+
+start https://www.youtube.com/watch?v=FoUWHfh733Y
+start https://en.kali.tools/
+exit
+
+:0
+exit
